eee965e89f85b840b9e5babfa250202eedbcdf95
王宇炜 <wyw0714@gmail.com>
2014-07-07 15:33:01 +0800

    no message

--- LinkedFiles/improvement.js
+++ LinkedFiles/improvement.js
@@ -1,18 +1,21 @@
 // JavaScript Document
-topBar = $('.top-bar');
+var topBar = $('.top-bar');
 topBar.css('height', 'auto');
 topBar.addClass('clear');
-var container = $('.box-c');
-var rightColumn = $(container.children()[0]);
-var centerColumn = $(container.children()[1]);
 $(window).resize(function() {
 	if (document.documentElement.clientWidth < 975) {
+		var container = $('.box-c');
+		var rightColumn = $(container.children()[0]);
+		var centerColumn = $(container.children()[1]);
 		container.prepend(centerColumn);
 		centerColumn.css('margin-right', '0px');
 		rightColumn.css('float', 'none');
 		rightColumn.css('width', 'auto');
 	}
 	else {
+		var container = $('.box-c');
+		var rightColumn = $(container.children()[0]);
+		var centerColumn = $(container.children()[1]);
 		container.prepend(rightColumn);
 		centerColumn.removeAttr('style');
 		rightColumn.removeAttr('style');
