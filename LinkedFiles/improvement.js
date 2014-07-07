79424e3e77c378c974a19e23a013098c2049acc1
王宇炜 <wyw0714@gmail.com>
2014-07-07 15:38:36 +0800

    no message

--- LinkedFiles/improvement.js
+++ LinkedFiles/improvement.js
@@ -5,8 +5,8 @@ topBar.addClass('clear');
 $(window).resize(function() {
 	if (document.documentElement.clientWidth < 975) {
 		var container = $('.box-c');
-		var rightColumn = $(container.children()[0]);
-		var centerColumn = $(container.children()[1]);
+		var rightColumn = $('.w38p');
+		var centerColumn = $('.mr39p');
 		container.prepend(centerColumn);
 		centerColumn.css('margin-right', '0px');
 		rightColumn.css('float', 'none');
@@ -14,8 +14,8 @@ $(window).resize(function() {
 	}
 	else {
 		var container = $('.box-c');
-		var rightColumn = $(container.children()[0]);
-		var centerColumn = $(container.children()[1]);
+		var rightColumn = $('.w38p');
+		var centerColumn = $('.mr39p');
 		container.prepend(rightColumn);
 		centerColumn.removeAttr('style');
 		rightColumn.removeAttr('style');
