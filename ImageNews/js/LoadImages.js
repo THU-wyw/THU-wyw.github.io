b9ffdfd815b96f65a24d1df038e50cf2713ae03c
王宇炜 <wyw0714@gmail.com>
2014-07-09 15:30:52 +0800

    no message

--- ImageNews/js/LoadImages.js
+++ ImageNews/js/LoadImages.js
@@ -1,41 +1,63 @@
 // JavaScript Document
-var Images;
-
+var images;
+var loaded;
+var current;
 function LoadImages() {
-	$.getJSON("album/PhotoList.txt", function(ImageList) {
-		for (var i = 0; i < ImageList.length; i++) {
+	$.getJSON("album/PhotoList.txt", function(imageList) {
+		loaded = new Array();
+		for (var i = 0; i < imageList.length; i++) {
 			var newPhoto = $('<li class="image-display"/>');
+			loaded[i] = false;
 			if (i == 0) {
 				newPhoto.css('display', 'block');
 			}
 			var img = new Image();
 			img.src = 'images/loading.png';
 			newPhoto.append(img);
-			newPhoto.append($('<span/>').text(ImageList[i].title));
+			newPhoto.append($('<span/>').text(imageList[i].title));
 			$('#image-list').append(newPhoto);
 		}
-		Images = new Array();
-		for (var i = 0; i < ImageList.length; i++) {
-			Images[i] = new Image();
-			Images[i].onload = (function(i, ImageList) {
-				if (i == ImageList.length - 1) {
+		current = 0;
+		images = new Array();
+		for (var i = 0; i < imageList.length; i++) {
+			images[i] = new Image();
+			images[i].onload = (function(i, imageList) {
+				if (i == imageList.length - 1) {
 					return function () {
 						$($($("#image-list").children()[i]).children()[0]).remove();
-						$($("#image-list").children()[i]).prepend(Images[i]);
+						$($("#image-list").children()[i]).prepend(images[i]);
+						loaded[i] = true;  
 						return;
 					}
 				}
 				else {
 					return function () {
 						$($($("#image-list").children()[i]).children()[0]).remove();
-						$($("#image-list").children()[i]).prepend(Images[i]);
-						Images[i + 1].src = ImageList[i + 1].imageUrl;
+						$($("#image-list").children()[i]).prepend(images[i]);
+						loaded[i] = true;
+						images[i + 1].src = imageList[i + 1].imageUrl;
 					}
 				}
-			})(i, ImageList);
+			})(i, imageList);
 		}
-		Images[0].src = ImageList[0].imageUrl;
+		images[0].src = imageList[0].imageUrl;
 	});
 }
 
 LoadImages();
+
+function setCurrentPhoto(value) {
+	$($('#image-list').children()[current]).removeAttr('style');
+	current = value;
+	$($('#image-list').children()[current]).css('display', 'block');
+}
+	
+function autoChange() {
+	if (loaded[(current + 1) % images.length]) {
+		setCurrentPhoto((current + 1) % images.length);
+	} 
+	setTimeout("autoChange()", 1000);
+}
+
+$('body').unload(autoChange);
+	
