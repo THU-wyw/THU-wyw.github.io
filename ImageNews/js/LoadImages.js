84416628f7470d8761c0389837ea466355f324c9
王宇炜 <wyw0714@gmail.com>
2014-07-09 15:42:20 +0800

    no message

--- ImageNews/js/LoadImages.js
+++ ImageNews/js/LoadImages.js
@@ -2,6 +2,7 @@
 var images;
 var loaded;
 var current;
+
 function LoadImages() {
 	$.getJSON("album/PhotoList.txt", function(imageList) {
 		loaded = new Array();
@@ -17,6 +18,7 @@ function LoadImages() {
 			newPhoto.append($('<span/>').text(imageList[i].title));
 			$('#image-list').append(newPhoto);
 		}
+		autoChange();
 		current = 0;
 		images = new Array();
 		for (var i = 0; i < imageList.length; i++) {
@@ -45,7 +47,71 @@ function LoadImages() {
 }
 
 LoadImages();
-
+/*
+loaded = new Array();
+imageList = [{
+"imageUrl": "album/1.jpg",
+"title": "图片1"
+}, {
+"imageUrl": "album/2.jpg",
+"title": "图片2"
+}, {
+"imageUrl": "album/3.jpg",
+"title": "图片3"
+}, {
+"imageUrl": "album/4.jpg",
+"title": "图片4"
+}, {
+"imageUrl": "album/5.jpg",
+"title": "图片5"
+}, {
+"imageUrl": "album/6.jpg",
+"title": "图片6"
+}, {
+"imageUrl": "album/7.jpg",
+"title": "图片7"
+}, {
+"imageUrl": "album/8.jpg",
+"title": "图片8"
+}];
+for (var i = 0; i < imageList.length; i++) {
+	var newPhoto = $('<li class="image-display"/>');
+	loaded[i] = false;
+	if (i == 0) {
+		newPhoto.css('display', 'block');
+	}
+	var img = new Image();
+	img.src = 'images/loading.png';
+	newPhoto.append(img);
+	newPhoto.append($('<span/>').text(imageList[i].title));
+	$('#image-list').append(newPhoto);
+}
+current = 0;
+images = new Array();
+for (var i = 0; i < imageList.length; i++) {
+	images[i] = new Image();
+	images[i].onload = (function(i, imageList) {
+		if (i == imageList.length - 1) {
+			return function () {
+				$($($("#image-list").children()[i]).children()[0]).remove();
+				$($("#image-list").children()[i]).prepend(images[i]);
+				loaded[i] = true;  
+				return;
+			}
+		}
+		else {
+			return function () {
+				$($($("#image-list").children()[i]).children()[0]).remove();
+				$($("#image-list").children()[i]).prepend(images[i]);
+				loaded[i] = true;
+				images[i + 1].src = imageList[i + 1].imageUrl;
+			}
+		}
+	})(i, imageList);
+}
+images[0].src = imageList[0].imageUrl;*/
+	
+	
 function setCurrentPhoto(value) {
 	$($('#image-list').children()[current]).removeAttr('style');
 	current = value;
@@ -59,5 +125,4 @@ function autoChange() {
 	setTimeout("autoChange()", 1000);
 }
 
-$('body').unload(autoChange);
 	
