9543840b054a68ba89d1cf207e09dbf3f4306ad1
王宇炜 <wyw0714@gmail.com>
2014-07-09 14:43:08 +0800

    no message

--- ImageNews/js/LoadImages.js
+++ ImageNews/js/LoadImages.js
@@ -1,10 +1,40 @@
 // JavaScript Document
-function LoadImageList() {
-	$.getJSON("album/PhotoList.txt", function(data) {
-	alert(data);
-});
-}
-function LoadImage() {
+var Images;
+
+function LoadImages() {
+	$.getJSON("album/PhotoList.txt", function(ImageList) {
+		for (var i = 0; i < ImageList.length; i++) {
+			var newPhoto = $('<li class="image-display"/>');
+			if (i == 0) {
+				newPhoto.css('display', 'block');
+			}
+			var img = new Image();
+			img.src = 'images/loading.png';
+			newPhoto.append(img);
+			newPhoto.append($('<span/>').text(ImageList[i].title));
+		}
+		Images = new Array();
+		for (var i = 0; i < ImageList.length; i++) {
+			Images[i] = new Image();
+			Images[i].onload = (function(i, ImageList) {
+				if (i == ImageList.length - 1) {
+					return function () {
+						$($($("#image-list").children()[i]).children()[0]).remove();
+						$($("#image-list").children()[i]).prepend(Images[i]);
+						return;
+					}
+				}
+				else {
+					return function () {
+						$($($("#image-list").children()[i]).children()[0]).remove();
+						$($("#image-list").children()[i]).prepend(Images[i]);
+						Images[i + 1].src = ImageList[i + 1].imageUrl;
+					}
+				}
+			})(i, ImageList);
+		}
+		Images[0].src = ImageList[0].imageUrl;
+	});
 }
 
-LoadImageList();
+LoadImages();
